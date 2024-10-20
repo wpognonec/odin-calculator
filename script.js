1aecc0583496307acf1bd0af0d505ef99bc55e80
William Pognonec <william.pognonec@gmail.com>
2024-10-20 10:40:12 -0500

    Added initial calculator functionality

--- script.js
+++ script.js
@@ -2,22 +2,34 @@ buttons = document.querySelectorAll("button")
 display = document.querySelector(".row1")
 display.textContent = 0
 
-let op1, op2, operator
+let op1, op2, operator, result
 
 function add(a, b) {
-  return a + b
+  result = a + b
+  op1 = undefined
+  op2 = undefined
+  return result
 }
 
 function subtract(a, b) {
-  return a - b
+  result = a - b
+  op1 = undefined
+  op2 = undefined
+  return result
 }
 
 function multiply(a, b) {
-  return a * b
+  result = a * b
+  op1 = undefined
+  op2 = undefined
+  return result
 }
 
 function divide(a, b) {
-  return a / b
+  result = a / b
+  op1 = undefined
+  op2 = undefined
+  return result
 }
 
 function operate(op1, op2, operator) {
@@ -37,11 +49,42 @@ function operate(op1, op2, operator) {
 
 buttons.forEach(button => {
   button.addEventListener("click", (e) => {
-    if (e.target.textContent === "AC") display.textContent = 0
+    if (e.target.textContent === "AC") {
+      display.textContent = 0
+      op1 = undefined
+      op2 = undefined
+      operator = undefined
+    }
     else if ("1234567890".includes(e.target.textContent)) {
       let digit = parseInt(e.target.textContent)
-      if (display.textContent === "0") display.textContent = digit
-      else display.textContent += digit
+      if (result && !op1) {
+        op1 = result
+      }
+      if (!op1) {
+        op1 = digit
+        display.textContent = op1
+      } else if (!op2 && !operator) {
+        op1 = op1 * 10 + digit
+        display.textContent = op1
+      } else if (!op2 && operator) {
+        op2 = digit
+        display.textContent = op2
+      } else {
+        op2 = op2 * 10 + digit
+        display.textContent = op2
+      }
+    } else if ("%/*-+".includes(e.target.textContent)) {
+      if (op1 && op2 && operator) {
+        result = operate(op1, op2, operator)
+        op1 = result
+        display.textContent = result
+      }
+      operator = e.target.textContent
+    } else if (e.target.textContent === "=") {
+      result = operate(op1, op2, operator)
+      op1 = result
+      operator = undefined
+      display.textContent = result
     }
   })
 });
